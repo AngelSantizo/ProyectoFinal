fe3eaf35eeb3f9dcdb1bf3b6c93c21ead1d79b0e
Angel Santizo <23001520@galileo.edu>
2023-02-20 21:00:16 -0600

    Introduccion al Problema

---
+++ ProyectoFinal.js
@@ -0,0 +1,97 @@
+var verirficacion = false;
+const PrecioBase = 2000; //precio base del plan
+var precioFinal = 0;
+
+while(verirficacion == false){ //Preguntamos al usuario su edad para verificacion
+
+    var edadAsegurado = parseInt(prompt("Ingresar Edad: "));
+    var preguntaAsegurado = prompt("Esta casado? responder Si/No; ")
+    var hijos = parseInt(prompt("Tiene hijos? Cuantos?, en caso de no tenerlos no responder: "));
+    var salario = parseInt(prompt("Ingresar cuanto gana al mes: "))
+    var Terrenos = parseInt(prompt("Cuantos terrenos tiene? en caso de no tener no responder: "))
+    var PrecioBase1 = 0;
+    var PrecioBase2 = 0;
+    var PrecioBase3 = 0;
+    var PrecioBase4 = 0;
+    var PrecioBase5 = 0;
+
+    if(edadAsegurado >= 18){ //la condicion para seguir es que sea mayor de 18 
+        var edadAsegurado = edadAsegurado
+        console.log(PrecioBase);
+    }else{
+        verirficacion = false
+        continue;
+    }
+
+    if(preguntaAsegurado == "Si" | preguntaAsegurado == "si" ){
+        
+        var edadConyuge = parseInt(prompt("Ingresar Edad de esposo/esposa: "));
+
+        if(edadConyuge >=18){ //verificamos si el esposo/esposa es mayor de 18
+            var edadConyuge = edadConyuge;
+
+            if(edadConyuge >= 18 & edadConyuge <=24){
+                var PrecioBase1 = parseInt((PrecioBase*0.10) + PrecioBase) //aca se hace el recargo del al conyuge 10%  
+            }
+            else if(edadConyuge >= 25 & edadConyuge <=49){
+                var PrecioBase1 = parseInt((PrecioBase*0.20) + PrecioBase) //aca del conyuge 20%\
+            }
+            else if(edadConyuge >= 50){
+                var PrecioBase1 = parseInt((PrecioBase*0.30) + PrecioBase) //aca del conyuge 30%
+            }
+        }else{
+            verirficacion = false
+            alert("Dato incorrecto, el/la conyuge no debe ser menor!")
+            continue;
+        }
+
+    }else if(preguntaAsegurado =="" | preguntaAsegurado !="Si"){
+        alert("No hay esposa/esposo");
+        console.log(PrecioBase);
+    }
+
+    if(hijos>=1){ //preguntamos si hubo respuesta en hijos  //AQUI HAY ERROR!!!
+        var PrecioBase2 = parseInt(((PrecioBase*0.20)*hijos) + PrecioBase);
+        var hijossi = true
+    }else{
+        var PrecioBase2 = 0;
+    }
+
+    if(edadAsegurado >= 18 & edadAsegurado <=24){
+        var PrecioBase3 = parseInt((PrecioBase*.10) + PrecioBase)//aca se hace el recargo del 10%  
+        console.log(PrecioBase3);
+    }
+    else if(edadAsegurado >= 25 & edadAsegurado <=49){
+        var PrecioBase3 = parseInt((PrecioBase*0.20) + PrecioBase) //aca del 20%
+    }
+    else if(edadAsegurado >= 50){
+        var PrecioBase3 = parseInt((PrecioBase*0.30) + PrecioBase) //aca del 30%
+    }
+
+    if(salario>=1){ //Hacemos la opoeracion por el salario devengado
+        var PrecioBase4 = salario*0.05;
+    }else{
+        var PrecioBase4 = 0;
+    }
+
+    if(Terrenos>=1){ //Hacemos la opreacion por cada terreno que tiene
+        var PrecioBase5 = (PrecioBase*0.35)*Terrenos;
+    }else{
+        var PrecioBase5 = 0;
+    }
+
+    var precioFinal = PrecioBase1 + PrecioBase2 + PrecioBase3 + PrecioBase4 + PrecioBase5; //hacemos la suma de todos los recargos
+
+    alert("El precio final es de: " + precioFinal);
+    var verificacion = prompt("Desea realizar otra cotizacion? Seguir/Salir: ") //preguntamos si el usuario desea realizar otra cotizacion 
+    
+    if(verificacion == "Si" | verificacion == "si"){
+        var verificacion = true
+    }else{
+        var verificacion = false
+        break;
+    }
+        //( ͠❛ ͜ʖ ͡❛) fin, a proyect powered by Angel Santizo
+} 
+
+console.log(PrecioBase1, PrecioBase2, PrecioBase3, PrecioBase4, PrecioBase5);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+## Requerimientos del programa:
+
+En el problema presentado, la empresa de seguros llamado Tk-U necesita que automaticemos el proceso de realizar cotizaciones, mediante un **programa** donde el usuario coloca sus datos, y estos mismos *afectan al total de la cotización*, esta información que afecta el precio son:
+
+1. **Edad**: Aqui el usuario coloca la edad que actualmente tiene, según el rango en el que se encuentre, se agrega un recargo.
+
+2. **Verificación si esta casado o no**: el usuario deberá mencionar si está casado o no, según la edad del cónyuge, cómo a la edad, se manejan los mismos rangos y se hacen recargo.
+
+3. **Hijos**: se verifica si el usuario cuenta con hijos, dependiendo la cantidad se hace un recargo por cada hijo/hija.
+
+4. **Cuanto es su salario**: el usuario ingresa la cantidad que gana al mes, dependiend de la cantidad, se le hace un recargo del 5%
+
+5. **Cuenta con propiedades**: preguntamos si el usuario cuenta con propiedades, de tener propiedades, ingresar la cantidad, dependiendo de la cantidad se le hace un recargo del 35% por cada una que cuente.
+
+En el código a utilizar, se hará varias *definiciones de variables y constantes*, en este caso el primer constante que no cambia es el precio base, luego las variables vendrían siendo todas aquellas preguntas y 5 datos que el usuario ingresa, podemos usar un prompt para pedir dicha información, ya que no son datos que son aplicables para todos los usuarios, no todos tienen la misma edad, no todos tienen esposa/esposo, hijos, terrenos, y n finalmente no todos ganan l mismo salario, **es por eso que estos datos varían**. 
+
+Según cada dato que vaya ingresando el usuario nosotros haremos la toma de decisiones, cuando si o cuando no aplicar dichos recargos, luego recopilamos dichos recargos, para que al final del programa, este haga la suma de los mismos, dando una cotización final al usuario.
 
